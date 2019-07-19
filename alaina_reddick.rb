0880d24f1fe5132685033bf5f491e6e478c812ee
Alaina Reddick <alainareddick@gmail.com>
2019-07-18 20:17:40 -0500

    commit 7/18
    class Suv
      def initialize (year, make, model, mileage, price)
        @year= year
        @make= make.upcase
        @model= model.upcase
        @mileage= mileage
        @price= price
      end
      def to_s
        "This is a #{@year} #{@make} #{@model}, with #{@mileage} miles and is priced at $#{@price}. "
    
      end
    end
    suv= Suv.new(1991,"Ford","escape","294,600",4455)
    puts suv
    suv= Suv.new(1998,"Chevy","Blazer","120,399",2342)
    puts suv
    suv= Suv.new(2003,"Kia", "Sportage","219,864",3213)
    puts suv
    suv= Suv.new(2009,"Dodge","Nitro","323,990",9933)
    puts suv
    suv= Suv.new(2010,"Chevy","Arcadia","200,019",9752)
    puts suv

--- alaina_reddick.rb
+++ alaina_reddick.rb
@@ -11,13 +11,13 @@ class Suv
 
   end
 end
-suv= Suv.new(1991,"Ford","escape",294600,4455)
+suv= Suv.new(1991,"Ford","escape","294,600",4455)
 puts suv
-suv= Suv.new(1998,"Chevy","Blazer",120399,2342)
+suv= Suv.new(1998,"Chevy","Blazer","120,399",2342)
 puts suv
-suv= Suv.new(2003,"Kia", "Sportage",219864,3213)
+suv= Suv.new(2003,"Kia", "Sportage","219,864",3213)
 puts suv
-suv= Suv.new(2009,"Dodge","Nitro",323990,9933)
+suv= Suv.new(2009,"Dodge","Nitro","323,990",9933)
 puts suv
-suv= Suv.new(2010,"Chevy","Arcadia",200019,9752)
+suv= Suv.new(2010,"Chevy","Arcadia","200,019",9752)
 puts suv
