245de6f199c5607d2e67b12bdf9373eb0b98a5d8
mik3teasdale <50425383+mik3teasdale@users.noreply.github.com>
2019-08-09 11:13:39 -0500

    Update test_file.rb

--- test_file.rb
+++ test_file.rb
@@ -1,13 +1,19 @@
 
+#create to seperate arrays
 car1 = ["car", "Ford", "Fusion"]
 car2 = ["car",'Toyota', 'corolla']
+#nest the arrays in another array
 array1 = [car1, car2]
 puts "#{array1}"
+#work with each nested array seperately
 array1.each do |car|
+  #Turn the array into a string
 array = car.join(' ')
 puts "\n"
 puts array
+  #remove the selected word 'car' in this case and assign to a variable
 array = array.gsub('car','')
 puts "\n"
+  #print out the vehicles to the console
 puts array.ljust(20)
 end
