df025efdfeb84dc0fcac20e3be7cda0ee8a07fc6
akrthf <49411278+akrthf@users.noreply.github.com>
2019-08-12 18:39:48 -0500

    Update alaina_reddick.rb

--- alaina_reddick.rb
+++ alaina_reddick.rb
@@ -1,23 +1,5 @@
-class Suv
-  def initialize (year, make, model, mileage, price)
-    @year= year
-    @make= make.upcase
-    @model= model.upcase
-    @mileage= mileage
-    @price= price
-  end
-  def to_s
-    "This is a #{@year} #{@make} #{@model}, with #{@mileage} miles and is priced at $#{@price}. "
-
-  end
-end
-suv= Suv.new(1991,"Ford","escape","294,600",4455)
-puts suv
-suv= Suv.new(1998,"Chevy","Blazer","120,399",2342)
-puts suv
-suv= Suv.new(2003,"Kia", "Sportage","219,864",3213)
-puts suv
-suv= Suv.new(2009,"Dodge","Nitro","323,990",9933)
-puts suv
-suv= Suv.new(2010,"Chevy","Arcadia","200,019",9752)
-puts suv
+@cars.new [suv,1991,"Ford","Escape","294,600", "an oldie but a goodie"]
+@cars.new [suv, 1998,"Chevy","Blazer","120,399","has exceptional mileage for the age"]
+@cars.new [suv,2003,"Kia", "Sportage","219,864","great first car"]
+@cars.new [suv,2009,"Dodge","Nitro","323,990","not too bad"]
+@cars.new [suv,2010,"Chevy","Arcadia","200,019","nice and cold air conditioner"]
