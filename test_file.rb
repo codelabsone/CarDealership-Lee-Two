d4d5a4f118ffe4e01e56ecd43d77cb550b3bef52
mik3teasdale <50425383+mik3teasdale@users.noreply.github.com>
2019-08-12 18:32:55 -0500

    Update test_file.rb

--- test_file.rb
+++ test_file.rb
@@ -1,19 +1,76 @@
 
-#create two seperate arrays
-car1 = ["car", "Ford", "Fusion"]
-car2 = ["car",'Toyota', 'corolla']
-#nest the arrays in another array
-array1 = [car1, car2]
-puts "#{array1}"
-#work with each nested array seperately
-array1.each do |car|
-  #Turn the array into a string
-array = car.join(' ')
-puts "\n"
-puts array
-  #remove the selected word 'car' in this case and assign to a variable
-array = array.gsub('car','')
-puts "\n"
-  #print out the vehicles to the console
-puts array.ljust(20)
+class Cars
+
+  attr_reader :category, :make, :model
+
+  def initialize(category,make,model)
+    @make = make
+    @model = model
+    @category = category
+  end
+  def to_s
+    "We have a #{@model}"
+  end
+end
+
+class Dealership
+  def initialize(name)
+    @name = name
+    @inventory = []
+  end
+  def add_car(car)
+    @inventory << car
+  end
+  def welcome
+    puts "welcome to #{@name}."
+    puts "To begin, we'd like to ask you a few questions:"
+  end
+end
+  def car_matcher
+    puts "What type of vehicle are you looking for?"
+    print 'We have '
+    @inventory.each do |c|
+      print "#{c.category}s. "
+      end
+      puts''
+      print ':  '
+      ans = gets.chomp
+        case ans
+        when 'car', 'cars'
+          puts cat_return(ans)
+        else
+          puts "I'm sorry but we don't have that type of vehicle"
+        end
+      end
+
+      module Fetcher
+
+        def self.fetch(car)
+
+        end
+      end
+
+
+
+
+
+
+if __FILE__ ==$0
+
+
+
+
+
+
+
+car1 = Cars.new("car", "Ford", "Fusion")
+car2 = Cars.new("car",'Toyota', 'corolla')
+lad_motors = Dealership.new("lad_m_motors")
+lad_motors.add_car(car1)
+lad_motors.add_car(car2)
+lad_motors.welcome
+lad_motors.car_matcher
+# puts car1.category
+# puts car1
+# puts car2
 end
